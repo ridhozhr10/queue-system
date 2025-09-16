0b5c4561a24f08f9a169c7b8dba8f159c0fefe04
ridhozhr10 <azharridho42@gmail.com>
2025-09-16 13:52:27 +0700

    wip: day2

---
+++ kiosk-ui/src/components/Layout.jsx
@@ -0,0 +1,88 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+
+const user = {
+  name: "Tom Cook",
+  email: "tom@example.com",
+  imageUrl:
+    "https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg",
+};
+
+const langDropdown = [
+  { name: "Indonesia", href: "#" },
+  { name: "English", href: "#" },
+];
+
+const serviceItems = ["Service1", "Service2", "Service3"];
+
+export default function Layout() {
+  return (
+    <>
+      <div className="min-h-full">
+        <nav className="bg-gray-800">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex items-center">
+                <div className="shrink-0">
+                  <img
+                    alt="Your Company"
+                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                    className="size-8"
+                  />
+                </div>
+              </div>
+              <div className="block">
+                <div className="ml-4 flex items-center md:ml-6">
+                  <Menu as="div" className="relative ml-3">
+                    <MenuButton className="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">Languange</span>
+                      <img
+                        alt=""
+                        src={user.imageUrl}
+                        className="size-8 rounded-full outline -outline-offset-1 outline-white/10"
+                      />
+                    </MenuButton>
+
+                    <MenuItems
+                      transition
+                      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-gray-800 py-1 outline-1 -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                    >
+                      {langDropdown.map((item) => (
+                        <MenuItem key={item.name}>
+                          <a
+                            href={item.href}
+                            className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:outline-hidden"
+                          >
+                            {item.name}
+                          </a>
+                        </MenuItem>
+                      ))}
+                    </MenuItems>
+                  </Menu>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <main className="h-screen flex items-center justify-center bg-gray-100">
+          <div className="w-full flex-wrap p-6 text-center">
+            <div>
+              <h1 className="text-6xl font-bold mb-4">Selamat Datang</h1>
+              <h1 className="text-2xl font-bold mb-4">
+                Silahkan Pilih Layanan
+              </h1>
+            </div>
+            <div className="grid grid-cols-3 gap-4 mt-6">
+              {serviceItems.map((item, i) => (
+                <div key={i + "item"} className="text-4xl">
+                  {item}
+                </div>
+              ))}
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
