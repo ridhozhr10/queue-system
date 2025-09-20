1f310cd0b4ca0bed1743e7a17c6b167bd80e7022
ridhozhr10 <azharridho42@gmail.com>
2025-09-20 13:18:28 +0700

    update join project

--- frontend/src/kiosk-ui/components/FormIntroduction.jsx
+++ frontend/src/kiosk-ui/components/FormIntroduction.jsx
@@ -31,9 +31,9 @@ export default function FormIntroduction({
             />
           ))}
           <div className="col-span-3">
-            <div class="mt-2">
-              <div class="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-gray-600 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-500">
-                <div class="shrink-0 text-base text-gray-400 select-none pr-2">
+            <div className="mt-2">
+              <div className="flex items-center rounded-md bg-white/5 pl-3 outline-1 -outline-offset-1 outline-gray-600 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-500">
+                <div className="shrink-0 text-base text-gray-400 select-none pr-2">
                   +62
                 </div>
                 <input
@@ -48,7 +48,7 @@ export default function FormIntroduction({
                   }}
                   value={formData.phone}
                   name="phone"
-                  class="block min-w-0 grow  py-1.5 pr-3 pl-1 text-base placeholder:text-gray-500 focus:outline-none sm:text-sm/6"
+                  className="block min-w-0 grow  py-1.5 pr-3 pl-1 text-base placeholder:text-gray-500 focus:outline-none sm:text-sm/6"
                 />
               </div>
             </div>
--- frontend/vite.kiosk.config.js
+++ frontend/vite.kiosk.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  publicDir: "../../public",
+  root: "src/kiosk-ui", // root folder
+  server: {
+    port: 5174, // unique port for kiosk
+  },
 });
